ba96e69990e1ad12fd32cefee40d607728c49d4c
Forbiddensequence <70473598+Forbiddensequence@users.noreply.github.com>
2021-08-31 18:38:01 +0300

    Update changevariable.cpp

--- changevariable.cpp
+++ changevariable.cpp
@@ -9,7 +9,7 @@ public:
   test1(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
-  void change(name user, uint64_t variable, std::string name) {
+  void change(name user, uint64_t variable) {
     require_auth( user );
 
     address_index addresses( get_self(), get_first_receiver().value );
@@ -19,14 +19,13 @@ public:
     check(iterator != addresses.end(), "Record does not exist");
     addresses.modify(iterator, user, [&]( auto& row ) {
         row.key = user;
-        row.name=name;
         row.variable=variable;
       });
     
   }
 
   [[eosio::action]]
-  void init(name user, uint64_t variable, std::string name) {
+  void init(name user, uint64_t variable) {
     require_auth( user );
 
     address_index addresses( get_self(), get_first_receiver().value );
@@ -35,7 +34,6 @@ public:
     
     addresses.emplace( user, [&]( auto& row ) {
         row.key = user;
-        row.name = name;
         row.variable = variable;
       });
     
@@ -51,7 +49,6 @@ public:
     check(iterator != addresses.end(), "Record does not exist");
     addresses.modify(iterator, user, [&]( auto& row ) {
         row.key = row.key;
-        row.name = row.name;
         row.variable = 0;
       });
   }
@@ -60,7 +57,6 @@ private:
   struct [[eosio::table]] person {
     name key;
     uint64_t variable;
-    std::string name;
     uint64_t primary_key() const { return key.value; }
   };
   using address_index = eosio::multi_index<"people"_n, person>;
